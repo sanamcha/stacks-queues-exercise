695a267ca18b4e5cc6cb5895f087b9b1c1560e0a
sanamcha <mhr.sanam@gmail.com>
2022-04-23 17:59:27 -0700

    final commit for stack

--- stack.js
+++ stack.js
@@ -20,26 +20,45 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
+    let newNode = new Node(val);
+    if(!this.last) this.last = newNode;
+    if(this.first) newNode.next = this.first; 
 
+    this.first = newNode;
+    this.size++;
+    return;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if(!this.first) {
+      throw new Error("Stack is Empty")
+    }
+    let firstVal = this.first.val;
+    if(this.first == this.last){
+      this.last = null;
+    }
+    this.first = this.first.next;
+    this.size--;
+    return firstVal;
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    if (this.size === 0) {
+      throw new Error("Stack is empty");
+    }
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    if(this.size === 0) return true;
+    return false;
   }
 }
 
