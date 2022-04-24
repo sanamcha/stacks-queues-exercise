24a4316fd4ef660a2cbc0e57cb993523a5db0686
sanamcha <mhr.sanam@gmail.com>
2022-04-23 17:59:06 -0700

    final commit for queue

--- queue.js
+++ queue.js
@@ -20,26 +20,40 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    let newNode = new Node(val)
+    if(!this.first) this.first = newNode;
+    if(this.last) this.last.next =newNode;
 
+    this.last = newNode;
+    this.size++;
+    return;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
+    if(this.size === 0){
+      throw new Error("Queue is Empty.. ")
+    }
+    let firstVal = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    return firstVal;
 
   }
 
   /** peek(): return the value of the first node in the queue. */
 
-  peek() {
-
+  peek() {  
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if(this.size === 0) return true;
+    else return false;
   }
 }
 
